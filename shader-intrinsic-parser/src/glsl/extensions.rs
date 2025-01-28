51cc31324e32830c51f9384fd582e79119fc9255
Antoine <aaltorffer@orange.fr>
2025-01-28 19:54:50 +0100

    Disable warning

--- shader-intrinsic-parser/src/glsl/extensions.rs
+++ shader-intrinsic-parser/src/glsl/extensions.rs
@@ -3,9 +3,11 @@ use shader_sense::symbols::symbols::{ShaderSymbol, ShaderSymbolList};
 use super::GlslIntrinsicParser;
 
 impl GlslIntrinsicParser {
+    #[allow(dead_code)]
     fn get_glsl_ext_mesh_shader(&self) -> ShaderSymbolList {
         // https://github.com/KhronosGroup/GLSL/blob/main/extensions/ext/GLSL_EXT_mesh_shader.txt
         let mut list = ShaderSymbolList::default();
+        #[allow(unreachable_code)]
         list.constants.push(ShaderSymbol {
             label: "gl_PrimitivePointIndicesEXT".into(),
             description: todo!(),
